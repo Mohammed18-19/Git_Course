04c72603447ec91719200375999d1a64fda467a9
mac <mac@MacBook-Pro-de-mac.local>
2025-06-29 10:23:28 +0100

    Deleting the wrong image

--- js/main.js
+++ js/main.js
@@ -11,13 +11,5 @@ const user = {
   }
 };
 
-// Function to greet the user
-function greetUser(user) {
-  console.log(`Hello, ${user.name}!`);
-  console.log(`You are ${user.age} years old and live in ${user.location}.`);
-  console.log(`Your top skill is: ${user.skills[0]}. Keep it up!`);
-  console.log(`Goal: ${user.education.goal}`);
-}
-
 // Call the function
 greetUser(user);
